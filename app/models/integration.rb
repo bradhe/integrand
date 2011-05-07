3ce2c7d260283e01ccdd3748f7bb18c0bd1b7d5c
Brad Heller <brad@aboutus.org>
2011-05-07 16:36:38 -0700

    Seperated our source repository from integration

--- app/models/integration.rb
+++ app/models/integration.rb
@@ -1,2 +1,7 @@
 class Integration < ActiveRecord::Base
+  has_one :source_repository
+
+  validates_presence_of :name
+  validates_presence_of :filesystem_path
+  validates_presence_of :source_control_path
 end
