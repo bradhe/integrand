4a5c0f02a80aff9e15388217833ba3add832257f
Brad Heller <brad@aboutus.org>
2011-05-07 20:12:36 -0700

    Added beginnings of source checkout crap

--- config/initializers/integrand.rb
+++ config/initializers/integrand.rb
@@ -1,3 +1,5 @@
-Dir[File.join("lib", "**/*.rb")].each do |f|
+Dir[File.join(Rails.root.join("lib"), "**/*.rb")].each do |f|
   require f
 end
+
+Integrand::Application.source_dir = File.dirname(__FILE__) + '/../../tmp'
--- lib/integrand.rb
+++ lib/integrand.rb
@@ -1,3 +1,11 @@
-
 module Integrand
+  class Application
+    def self.source_dir
+      @@source_dir
+    end
+
+    def self.source_dir=(dir)
+      @@source_dir = dir
+    end
+  end
 end
