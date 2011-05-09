97fd48bf8dc518423d75ac1b66ccb58d6a50d782
Brad Heller <brad@aboutus.org>
2011-05-08 20:00:55 -0700

    Ready to rock.

--- lib/integrand/command_line.rb
+++ lib/integrand/command_line.rb
@@ -1,11 +1,18 @@
 require 'open3'
+require 'tempfile'
 
 module Integrand::CommandLine
   def chdir(path)
     Dir.chdir path
   end
 
-  def run_command(cmd, *args, &blk)
-    Open3.popen3(cmd, args, blk) { |i,o,e| blk.call(o) }
+  def run_command(cmd, &blk)
+    tmp = Tempfile.new
+
+    cmd.split("\n").reject { |s| s =~ /\s+/ }.each do |c|
+      Open3.popen3(c) { |i,o,e| tmp.write(o.read) }
+    end
+
+    blk.call(tmp) if blk
   end
 end
